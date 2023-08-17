7b4df03643ed581e3eb7f8923ed973490684f32c
Max <ienergystaff@gmail.com>
2023-08-17 22:43:12 +0300

    Update user.py

--- telegram/handlers/user.py
+++ telegram/handlers/user.py
@@ -16,13 +16,13 @@ bot.add_custom_filter(ChatFilter())
 def usage_command(message):
     username = extract_arguments(message.text)
     if not username:
-        return bot.reply_to(message, 'Usage: `/usage <username>`', parse_mode='MarkdownV2')
+        return bot.reply_to(message, 'Использование: `/usage <username>`', parse_mode='MarkdownV2')
 
     with GetDB() as db:
         dbuser = crud.get_user(db, username)
 
         if not dbuser:
-            return bot.reply_to(message, "No user found with this username")
+            return bot.reply_to(message, "Пользователь с таким именем не найден")
         user = UserResponse.from_orm(dbuser)
 
         statuses = {
@@ -35,10 +35,10 @@ def usage_command(message):
 ┌─{statuses[user.status]} <b>Status:</b> <code>{user.status.title()}</code>
 │          └─<b>Username:</b> <code>{user.username}</code>
 │
-├─🔋 <b>Data limit:</b> <code>{readable_size(user.data_limit) if user.data_limit else 'Unlimited'}</code>
-│          └─<b>Data Used:</b> <code>{readable_size(user.used_traffic) if user.used_traffic else "-"}</code>
+├─🔋 <b>Ограничение данных:</b> <code>{readable_size(user.data_limit) if user.data_limit else 'Unlimited'}</code>
+│          └─<b>Использованно данных:</b> <code>{readable_size(user.used_traffic) if user.used_traffic else "-"}</code>
 │
-└─📅 <b>Expiry Date:</b> <code>{datetime.fromtimestamp(user.expire).date() if user.expire else 'Never'}</code>
-            └─<b>Days left:</b> <code>{(datetime.fromtimestamp(user.expire or 0) - datetime.now()).days if user.expire else '-'}</code>'''
+└─📅 <b>Срок действия:</b> <code>{datetime.fromtimestamp(user.expire).date() if user.expire else 'Never'}</code>
+            └─<b>Осталось дней:</b> <code>{(datetime.fromtimestamp(user.expire or 0) - datetime.now()).days if user.expire else '-'}</code>'''
 
     return bot.reply_to(message, text, parse_mode='HTML')
